849d35b77f8f9e9a43621faf7fbc8f2fd57d58f2
swamiputhineedi-web <swamiputhineedi8@gmail.com>
2026-03-16 11:53:16 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+const text = ["Web Developer", "ECE Student", "Programmer"];
+
+let count = 0;
+let index = 0;
+let currentText = "";
+let letter = "";
+
+function type(){
+
+if(count === text.length){
+count = 0;
+}
+
+currentText = text[count];
+letter = currentText.slice(0, ++index);
+
+document.getElementById("typing").textContent = letter;
+
+if(letter.length === currentText.length){
+count++;
+index = 0;
+}
+
+setTimeout(type,200);
+}
+
+type();
